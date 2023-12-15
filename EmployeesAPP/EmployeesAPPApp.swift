5f1f033eb1ebd1525d1adc3273e688f516fdfc68
gliadev <gliatem@gmail.com>
2023-12-15 23:09:12 +0100

    Initial Commit

---
+++ EmployeesAPP/EmployeesAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeesAPPApp.swift
+//  EmployeesAPP
+//
+//  Created by Adolfo on 15/12/23.
+//
+
+import SwiftUI
+
+@main
+struct EmployeesAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
